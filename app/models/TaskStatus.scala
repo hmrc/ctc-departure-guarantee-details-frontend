cef3caf85bfb033a1efd56676026293f2b7a3796
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2023-09-08 08:40:58 +0100

    CTCP-3724: Handle unavailable status

--- app/models/TaskStatus.scala
+++ app/models/TaskStatus.scala
@@ -19,29 +19,48 @@ package models
 import play.api.libs.json.{JsError, JsString, JsSuccess, JsValue, Reads, Writes}
 
 sealed trait TaskStatus {
+  val messageKey: String
+  val tag: String
   val jsonString: String
-  def isCompleted: Boolean = this == TaskStatus.Completed
+  def isCompleted: Boolean   = this == TaskStatus.Completed
+  def isUnavailable: Boolean = this == TaskStatus.Unavailable
 }
 
 object TaskStatus {
 
   case object Completed extends TaskStatus {
+    override val messageKey: String = "taskStatus.completed"
+    override val tag: String        = "govuk-tag--green"
     override val jsonString: String = "completed"
   }
 
   case object InProgress extends TaskStatus {
+    override val messageKey: String = "taskStatus.inProgress"
+    override val tag: String        = "govuk-tag--blue"
     override val jsonString: String = "in-progress"
   }
 
   case object NotStarted extends TaskStatus {
+    override val messageKey: String = "taskStatus.notStarted"
+    override val tag: String        = "govuk-tag--grey"
     override val jsonString: String = "not-started"
   }
 
   case object CannotStartYet extends TaskStatus {
+    override val messageKey: String = "taskStatus.cannotStartYet"
+    override val tag: String        = "govuk-tag--red"
     override val jsonString: String = "cannot-start-yet"
   }
 
+  case object Unavailable extends TaskStatus {
+    override val messageKey: String = "taskStatus.completed"
+    override val tag: String        = "govuk-tag--green"
+    override val jsonString: String = "unavailable"
+  }
+
   case object Error extends TaskStatus {
+    override val messageKey: String = "taskStatus.error"
+    override val tag: String        = "govuk-tag--red"
     override val jsonString: String = "error"
   }
 
@@ -52,6 +71,7 @@ object TaskStatus {
       case NotStarted.jsonString     => JsSuccess(NotStarted)
       case CannotStartYet.jsonString => JsSuccess(CannotStartYet)
       case Error.jsonString          => JsSuccess(Error)
+      case Unavailable.jsonString    => JsSuccess(Unavailable)
       case x                         => JsError(s"$x is not a valid task status")
     }
   }
