b5089b6fb0c60f44effffe5a49faaa602bdbf7b0
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2023-09-08 08:42:17 +0100

    CTCP-3742: Removed unused overrides

--- app/models/TaskStatus.scala
+++ app/models/TaskStatus.scala
@@ -19,8 +19,6 @@ package models
 import play.api.libs.json.{JsError, JsString, JsSuccess, JsValue, Reads, Writes}
 
 sealed trait TaskStatus {
-  val messageKey: String
-  val tag: String
   val jsonString: String
   def isCompleted: Boolean   = this == TaskStatus.Completed
   def isUnavailable: Boolean = this == TaskStatus.Unavailable
@@ -29,38 +27,26 @@ sealed trait TaskStatus {
 object TaskStatus {
 
   case object Completed extends TaskStatus {
-    override val messageKey: String = "taskStatus.completed"
-    override val tag: String        = "govuk-tag--green"
     override val jsonString: String = "completed"
   }
 
   case object InProgress extends TaskStatus {
-    override val messageKey: String = "taskStatus.inProgress"
-    override val tag: String        = "govuk-tag--blue"
     override val jsonString: String = "in-progress"
   }
 
   case object NotStarted extends TaskStatus {
-    override val messageKey: String = "taskStatus.notStarted"
-    override val tag: String        = "govuk-tag--grey"
     override val jsonString: String = "not-started"
   }
 
   case object CannotStartYet extends TaskStatus {
-    override val messageKey: String = "taskStatus.cannotStartYet"
-    override val tag: String        = "govuk-tag--red"
     override val jsonString: String = "cannot-start-yet"
   }
 
   case object Unavailable extends TaskStatus {
-    override val messageKey: String = "taskStatus.completed"
-    override val tag: String        = "govuk-tag--green"
     override val jsonString: String = "unavailable"
   }
 
   case object Error extends TaskStatus {
-    override val messageKey: String = "taskStatus.error"
-    override val tag: String        = "govuk-tag--red"
     override val jsonString: String = "error"
   }
 
