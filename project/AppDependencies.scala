4df5f57372d9d06dee2801822e24289e92773a29
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2024-10-16 14:36:55 +0100

    CTCP-5963: Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.0.0"
+  private val bootstrapVersion = "9.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "10.3.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "3.1.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "10.13.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"   % "3.2.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion,
     "org.typelevel"     %% "cats-core"                      % "2.12.0"
   )
@@ -19,7 +19,7 @@ object AppDependencies {
     "org.scalatestplus"       %% "mockito-4-11"            % "3.2.18.0",
     "org.scalacheck"          %% "scalacheck"              % "1.18.0",
     "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.18.0",
-    "org.jsoup"               %  "jsoup"                   % "1.17.2",
+    "org.jsoup"               %  "jsoup"                   % "1.18.1",
     "io.github.wolfendale"    %% "scalacheck-gen-regexp"   % "1.1.0"
   ).map(_ % "test")
 
